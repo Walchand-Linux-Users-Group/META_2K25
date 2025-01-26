969207ed182a534284c7bc0e1cb2471c39bed665
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-01-26 16:07:29 +0530

    Docker 3d element and cards added

---
+++ src/components/registerPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Docker from "./docker";
+import ThreeDModel from "./docker";
+
+export default function RegisterPage(){
+    return(
+        <>
+        <div className="bg-black ">
+
+            
+            <Docker className=""/>
+        
+
+        </div>
+        
+        </>
+    )
+}
