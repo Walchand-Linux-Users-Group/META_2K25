25e7a762438233014da5cff7a7d7f214790bb940
tanmay-8 <tanmayshingde8@gmail.com>
2025-01-29 21:44:36 +0530

    registration

--- src/components/registerPage.jsx
+++ src/components/registerPage.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import Docker from "./docker";
 import ThreeDModel from "./docker";
+import Register from "./register";
 
-export default function RegisterPage(){
-    return(
-        <>
-        <div className="bg-black ">
-
-            
-            <Docker className=""/>
-        
-
+export default function RegisterPage() {
+    return (
+        <div className="bg-[#020519]">
+            <Docker />
+            <Register />
         </div>
-        
-        </>
-    )
+    );
 }
