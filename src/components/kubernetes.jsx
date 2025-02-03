8400b49ea544994fd9391e72395fe4c91d1d8f5e
PranavG1203 <pranavg2016@gmail.com>
2025-02-03 15:31:43 +0530

    scroll

--- src/components/kubernetes.jsx
+++ src/components/kubernetes.jsx
@@ -23,7 +23,7 @@ export default function Kubernetes() {
               <div>
                 <SiKubernetes className=" text-[#4879e2] fhd:text-[110px] lg:text-[60px] md:text-[50px] text-6xl mx-auto" />
                 <h2 className="text-xl fhd:text-3xl lg:text-lg md:text-sm  text-center font-bold mt-3">
-                  Session 3 <br /> Sailing to k8s
+                  Session 3 <br /> Sailing to K8s
                 </h2>
               </div>
               <p className="fhd:text-lg text-sm lg:text-sm md:text-[15px] text-justify mt-5">
@@ -50,24 +50,22 @@ export default function Kubernetes() {
                 <div className="h-5"></div>
               </div>
               <p className="fhd:text-lg text-sm lg:text-sm md:text-[15px] text-justify mt-5 ">
-                Level up your Kubernetes skills! 🚀 Learn Pods, Services,
+                Level up your Kubernetes skills! Learn Pods, Services,
                 Deployments and more to automate and scale applications
                 seamlessly. Go beyond theory with real-world projects, deploying
                 and managing cloud-native apps like a pro. For the grand
-                finale—dive into epic wargames! 🔥 Test your skills, compete and
-                push your Docker & K8s expertise to the limit! 🎯
+                finale—dive into epic wargames! Test your skills, compete and
+                push your Docker & K8s expertise to the limit!
               </p>
             </div>
           </div>
           <div className=" px-10 -bottom-10  md:px-0 h-[550px] md:h-auto w-full md:w-2/6 flex overflow-hidden flex-col items-center relative pb-3">
-
             <img
               src="base.png"
               alt="model base image"
               className="absolute  top-24 fhd:top-32 lg:top-20 md:top-40 pointer-events-none w-full max-w-[350px] md:max-w-[350px] fhd:max-w-[700px] lg:max-w-[375px] animate-flicker"
             />
             <KubernetesModel className="cursor-pointer relative z-50 pb-10 xl:right-0.5" />
-
           </div>
         </div>
       </div>
