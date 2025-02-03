e6b06f0a922ef2a8bae95369026b8edc79a3fdf9
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-02-03 10:13:56 +0530

    Some changes

--- src/components/registerPage.jsx
+++ src/components/registerPage.jsx
@@ -4,15 +4,16 @@ import React, { useState, useEffect, useRef } from "react";
     import ThreeDModel from "./docker";
 import Kubernetes from "./kubernetes";
 import "../css/register.css"
-<<<<<<< HEAD
 import ParticleBackground from "./particles";
 import Register from "./register";
 import  Footer  from "./Footer";
+import RegisterTitle from "./registerTitle";
 
 export default function RegisterPage() {
     return (
         <>
             <div className="bg-[#000] relative h-full  overflow-x-hidden overflow-y-auto space-y-20 py-4">
+                <RegisterTitle/>
                 <ParticleBackground />
                 <Docker />
                 <Kubernetes />
@@ -21,24 +22,5 @@ export default function RegisterPage() {
             </div>
         </>
     );
-=======
-import ParticleBackground from "./particles"
-import Register from "./register"
-import Footer from "./Footer"
-import RegisterTitle from "./registerTitle";
 
-export default function RegisterPage() {
-  return (
-    <div className="relative min-h-screen w-full bg-black overflow-x-hidden">
-      <ParticleBackground  />
-      <div className="relative z-10 space-y-20 py-4">
-        <RegisterTitle/>
-        <Docker />
-        <Kubernetes />
-        <Register />
-        <Footer />
-      </div>
-    </div>
-  )
->>>>>>> 4ffe48976a246b7f3d8dfa67c8fab167439c3d17
 }
